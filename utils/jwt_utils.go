586ff90793163de76dc19a933b2523a6e4bab4af
Daffadon <daffaputranarendra9@gmail.com>
2024-07-12 19:38:26 +0700

    feat: add jwt utils function

---
+++ utils/jwt_utils.go
@@ -0,0 +1,53 @@
+package utils
+
+import (
+	"os"
+	"time"
+
+	"log"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/joho/godotenv"
+)
+
+type Claims struct {
+	UserId uint `json:"userId"`
+	jwt.StandardClaims
+}
+
+func GenerateToken(userId uint) (string, error) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	jwtKey := []byte(os.Getenv("JWT_SECRET_KEY"))
+	expirationTime := time.Now().Add(30 * time.Minute)
+
+	claims := &Claims{
+		UserId: userId,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtKey)
+}
+func ValidateJWT(tokenStr string) (*Claims, error) {
+	jwtKey := []byte(os.Getenv("JWT_SECRET_KEY"))
+	claims := &Claims{}
+
+	token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			return nil, err
+		}
+		return nil, err
+	}
+	if !token.Valid {
+		return nil, err
+	}
+	return claims, nil
+}
